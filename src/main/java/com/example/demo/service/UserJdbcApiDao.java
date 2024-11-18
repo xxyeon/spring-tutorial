70106828fddc2d1a8684da9fb710db6bedd1a3af
xxyeon <jykim9335@gmail.com>
2024-11-18 14:16:04 +0900

    SQL Injection 방지하기 위해 statement 대신 prepareStatement사 (전체/단일조회)

--- src/main/java/com/example/demo/service/UserJdbcApiDao.java
+++ src/main/java/com/example/demo/service/UserJdbcApiDao.java
@@ -1,10 +1,7 @@
 package com.example.demo.service;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,27 +16,17 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class UserJdbcApiDao {
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-    @Value("${spring.datasource.driver-class-name}")
-    private String driver;
-
     private final DataSource dataSource;
 
     public User findById(int userId) throws SQLException {
-        Connection connection = null;   // 1
-        Statement statement = null;     // 2
-        ResultSet resultSet = null;     // 3
+        Connection connection = null;           // 1
+        PreparedStatement statement = null;     // 2
+        ResultSet resultSet = null;             // 3
         try {
-            connection = dataSource.getConnection(); // 1 pool에서 커넥션을 가져오므로 url, username, password를 매번 넣지 않아도됨
-            statement = connection.createStatement();   // 2
-            resultSet = statement.executeQuery(         // 3
-                    "SELECT * FROM \"user\" WHERE id = " + userId //쿼리 결과가 resultSet에 담김
-            );
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement("SELECT * FROM \"user\" WHERE id = ?");
+            statement.setInt(1, userId);
+            resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 return new User(
                         resultSet.getInt("id"),
@@ -54,7 +41,7 @@ public class UserJdbcApiDao {
                 );
             }
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "유저 정보가 존재하지 않습니다 - id : " + userId);
-        } catch (SQLException e) { //checked exception -> 컴파일 에러
+        } catch (SQLException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
             // 자원반납
@@ -65,15 +52,13 @@ public class UserJdbcApiDao {
     }
 
     public List<User> findAll() throws SQLException {
-        Connection connection = null;   // 1
-        Statement statement = null;     // 2
-        ResultSet resultSet = null;     // 3
+        Connection connection = null;           // 1
+        PreparedStatement statement = null;     // 2
+        ResultSet resultSet = null;             // 3
         try {
-            connection = dataSource.getConnection();    // 1
-            statement = connection.createStatement();   // 2
-            resultSet = statement.executeQuery(         // 3
-                    "SELECT * FROM \"user\""
-            );
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement("SELECT * FROM \"user\"");
+            resultSet = statement.executeQuery();
             List<User> results = new ArrayList();
             while (resultSet.next()) {
                 results.add(
