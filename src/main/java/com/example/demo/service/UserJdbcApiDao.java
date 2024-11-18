dbcf24f95691ddd1aba34d1958550afff6516188
xxyeon <jykim9335@gmail.com>
2024-11-18 14:12:46 +0900

    JDBC API를 통한 전체 유저 조회

--- src/main/java/com/example/demo/service/UserJdbcApiDao.java
+++ src/main/java/com/example/demo/service/UserJdbcApiDao.java
@@ -12,6 +12,8 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Repository
@@ -53,6 +55,43 @@ public class UserJdbcApiDao {
             }
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "유저 정보가 존재하지 않습니다 - id : " + userId);
         } catch (SQLException e) { //checked exception -> 컴파일 에러
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
+        } finally {
+            // 자원반납
+            if (resultSet != null) resultSet.close();   // 1
+            if (statement != null) statement.close();   // 2
+            if (connection != null) connection.close(); // 3
+        }
+    }
+
+    public List<User> findAll() throws SQLException {
+        Connection connection = null;   // 1
+        Statement statement = null;     // 2
+        ResultSet resultSet = null;     // 3
+        try {
+            connection = dataSource.getConnection();    // 1
+            statement = connection.createStatement();   // 2
+            resultSet = statement.executeQuery(         // 3
+                    "SELECT * FROM \"user\""
+            );
+            List<User> results = new ArrayList();
+            while (resultSet.next()) {
+                results.add(
+                        new User(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name"),
+                                resultSet.getInt("age"),
+                                resultSet.getString("job"),
+                                resultSet.getString("specialty"),
+                                resultSet.getTimestamp("created_at")
+                                        .toInstant()
+                                        .atZone(ZoneId.systemDefault())
+                                        .toLocalDateTime()
+                        )
+                );
+            }
+            return results;
+        } catch (SQLException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
             // 자원반납
