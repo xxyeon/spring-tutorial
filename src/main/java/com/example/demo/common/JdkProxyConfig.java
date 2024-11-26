b37e2f16ede8ca4dfba4cd4e8a5a08770761fb7e
xxyeon <jykim9335@gmail.com>
2024-11-26 15:20:40 +0900

    fix: IUserService 빈 사이클 오류

--- src/main/java/com/example/demo/common/JdkProxyConfig.java
+++ src/main/java/com/example/demo/common/JdkProxyConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.common;
 
 import com.example.demo.service.IUserService;
+import com.example.demo.service.UserService;
 import com.example.demo.service.proxy.JdkProxyHandler;
 import java.lang.reflect.Proxy;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +14,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class JdkProxyConfig {
     //프록시를 직접 구현해주지 않아도된다. Java Reflection API 활용
     //UserServiced(target), PlatfromTransactionManager(부가기능) 만 명시해서 이 둘을 가지고 프록시 생성해주도록
-    private final IUserService userService;
+    private final UserService userService;
     private final PlatformTransactionManager transactionManager;
 
     @Bean //IUserService 타입으로 userServiceJdkProxy 이름의 빈이 등록 -> 컨트롤러에서 사용
